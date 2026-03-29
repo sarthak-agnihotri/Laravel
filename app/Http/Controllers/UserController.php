b29a686d29df354d689299c5d6f0699fe28980ed
Sarthak <sarthak0144@gmail.com>
2026-03-29 15:10:54 +0530

    comment

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,6 +21,6 @@ class UserController extends Controller
     }
 
     function adminlogin(){
-        return view('admin.login');
+        return view('admin.login');//inside view->admin->login
     }
 }
